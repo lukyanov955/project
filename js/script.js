99fc8b42e0904d9347e5555ad3b3efa3b69cf05a
lukyanov955 <lukyanov955@yandex.kz>
2024-06-28 12:38:47 +0500

    1

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@ document.getElementById('circleIcon').addEventListener('click', function() {
     var tileMenu = document.getElementById('tileMenu');
     if (tileMenu.style.display === 'none' || tileMenu.style.display === '') {
         tileMenu.style.display = 'grid';
+        // Добавляем анимацию плавного появления плиток
+        tileMenu.style.opacity = '0';
+        setTimeout(function() {
+            tileMenu.style.opacity = '1';
+            tileMenu.style.transition = 'opacity 0.3s';
+        }, 10);
     } else {
-        tileMenu.style.display = 'none';
+        tileMenu.style.opacity = '0';
+        setTimeout(function() {
+            tileMenu.style.display = 'none';
+        }, 300); // Время должно совпадать с продолжительностью анимации
     }
 });
